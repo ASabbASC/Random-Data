b0f64971ec331183e925f34d1cf971d1dc760223
ASabbASC <52670527+ASabbASC@users.noreply.github.com>
2019-08-09 14:29:57 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,357 @@
+const data = {
+  results: [
+    {
+      gender: "female",
+      name: { title: "miss", first: "matilda", last: "wiita" },
+      location: {
+        street: "9452 rotuaari",
+        city: "juuka",
+        state: "lapland",
+        postcode: 73892,
+        coordinates: { latitude: "-8.7253", longitude: "37.8363" },
+        timezone: {
+          offset: "+9:00",
+          description: "Tokyo, Seoul, Osaka, Sapporo, Yakutsk"
+        }
+      },
+      email: "matilda.wiita@example.com",
+      login: {
+        uuid: "8f3efb34-bdc0-4ff3-935c-70d77469ff24",
+        username: "silverkoala148",
+        password: "drummer",
+        salt: "1Wd97puo",
+        md5: "92c332d7ccf867fe74f1749579e7cc1d",
+        sha1: "22d741a50a0d2a631ad4fdf36209a03baac1cb58",
+        sha256:
+          "45ed7e9e3364b3af32a9e483f584f3a7764c4943e311c16207a58e2bcc5ed7f0"
+      },
+      dob: { date: "1988-07-20T22:07:04Z", age: 31 },
+      registered: { date: "2009-11-13T21:36:14Z", age: 9 },
+      phone: "04-359-913",
+      cell: "041-726-51-97",
+      id: { name: "HETU", value: "NaNNA146undefined" },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/43.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/43.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/43.jpg"
+      },
+      nat: "FI"
+    },
+    {
+      gender: "female",
+      name: { title: "mrs", first: "malou", last: "larsen" },
+      location: {
+        street: "5222 degnevænget",
+        city: "samsø",
+        state: "danmark",
+        postcode: 11014,
+        coordinates: { latitude: "-62.2932", longitude: "80.3797" },
+        timezone: { offset: "-2:00", description: "Mid-Atlantic" }
+      },
+      email: "malou.larsen@example.com",
+      login: {
+        uuid: "3a92fc27-a4e9-4795-81b1-0989ffcbc33f",
+        username: "bluezebra283",
+        password: "cobalt",
+        salt: "KFJ98WTX",
+        md5: "e74fbc5a2f13c2247611fe137169f54a",
+        sha1: "7117039a36160be9f729a308da4bd1f41cdb8bf9",
+        sha256:
+          "03986a3b650c0d3277aa0e8d8317f4893481b22870e2fd579cdfeced44e299ae"
+      },
+      dob: { date: "1967-07-09T22:06:49Z", age: 52 },
+      registered: { date: "2013-07-23T20:46:00Z", age: 6 },
+      phone: "14749290",
+      cell: "96773179",
+      id: { name: "CPR", value: "327454-5161" },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/58.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/58.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/58.jpg"
+      },
+      nat: "DK"
+    },
+    {
+      gender: "female",
+      name: { title: "mrs", first: "elma", last: "strauch" },
+      location: {
+        street: "friedhofstraße 119",
+        city: "kellinghusen",
+        state: "schleswig-holstein",
+        postcode: 53976,
+        coordinates: { latitude: "-53.3750", longitude: "-24.1384" },
+        timezone: { offset: "+5:45", description: "Kathmandu" }
+      },
+      email: "elma.strauch@example.com",
+      login: {
+        uuid: "b926b7f4-e0fa-4bc4-abf6-3c68782a23c4",
+        username: "happylion431",
+        password: "nevets",
+        salt: "t14aOYBa",
+        md5: "98495ca3c908791e998a7a1026bcfd3c",
+        sha1: "de6f1490454535c6d59c8ecfaa1fb10af88b391a",
+        sha256:
+          "0dcd0d811e7d38b3e955f47ac81bc775ca4ba81fa3afac0f6c9b27e0d862a43b"
+      },
+      dob: { date: "1947-02-13T03:25:54Z", age: 72 },
+      registered: { date: "2005-06-25T14:26:51Z", age: 14 },
+      phone: "0351-0181975",
+      cell: "0174-0128523",
+      id: { name: "", value: null },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/68.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/68.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/68.jpg"
+      },
+      nat: "DE"
+    },
+    {
+      gender: "male",
+      name: { title: "mr", first: "friedhold", last: "siegl" },
+      location: {
+        street: "talstraße 83",
+        city: "doberlug-kirchhain",
+        state: "rheinland-pfalz",
+        postcode: 32074,
+        coordinates: { latitude: "67.2880", longitude: "49.6957" },
+        timezone: { offset: "-9:00", description: "Alaska" }
+      },
+      email: "friedhold.siegl@example.com",
+      login: {
+        uuid: "b956e75d-e0ad-470a-a850-3b68202e1fb8",
+        username: "angryswan723",
+        password: "holly1",
+        salt: "6jlVc8oh",
+        md5: "38a9c0fd0d017862afbe146e2bb07e4f",
+        sha1: "c26593d1965fe2c0fe57a1b89424859e309750df",
+        sha256:
+          "cd4501b727ca844fa80ed9e6b7146b54195faf91799127492453a9e2484c686b"
+      },
+      dob: { date: "1964-09-01T22:09:45Z", age: 54 },
+      registered: { date: "2017-05-24T13:54:47Z", age: 2 },
+      phone: "0358-4673511",
+      cell: "0174-8474140",
+      id: { name: "", value: null },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/34.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/34.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/34.jpg"
+      },
+      nat: "DE"
+    },
+    {
+      gender: "female",
+      name: { title: "miss", first: "miriam", last: "fernandez" },
+      location: {
+        street: "8823 avenida de salamanca",
+        city: "alcobendas",
+        state: "castilla la mancha",
+        postcode: 81289,
+        coordinates: { latitude: "-2.6663", longitude: "-146.8694" },
+        timezone: { offset: "-1:00", description: "Azores, Cape Verde Islands" }
+      },
+      email: "miriam.fernandez@example.com",
+      login: {
+        uuid: "14c94594-04d1-4ce3-a3e2-2c0f3170db6e",
+        username: "bigbutterfly847",
+        password: "manning",
+        salt: "Eiuelc21",
+        md5: "08dbb891f10e090ba26d1e30b8fc7042",
+        sha1: "347e8f4bb5b11b0f4ad66e403a8354e5cf5cf9db",
+        sha256:
+          "46f4e96c34c7f5309948717abad03499115da2cb5427e8d6fd0bc3ef5615d74b"
+      },
+      dob: { date: "1959-03-06T21:30:50Z", age: 60 },
+      registered: { date: "2002-10-26T17:30:58Z", age: 16 },
+      phone: "968-824-861",
+      cell: "673-794-442",
+      id: { name: "DNI", value: "14805547-U" },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/93.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/93.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/93.jpg"
+      },
+      nat: "ES"
+    },
+    {
+      gender: "female",
+      name: { title: "ms", first: "gladys", last: "burns" },
+      location: {
+        street: "1158 adams st",
+        city: "shiloh",
+        state: "minnesota",
+        postcode: 53426,
+        coordinates: { latitude: "-28.6748", longitude: "-52.6394" },
+        timezone: { offset: "-11:00", description: "Midway Island, Samoa" }
+      },
+      email: "gladys.burns@example.com",
+      login: {
+        uuid: "9e7ac692-27e6-4b72-b066-ea436e204c72",
+        username: "whitefrog195",
+        password: "stoned",
+        salt: "qms624XN",
+        md5: "5b65d3dbc32301ff8c73e7a03431db60",
+        sha1: "5aa3ad20b18b36dc03a37e756aad76e73c632f30",
+        sha256:
+          "1049279bea8d6e5f42beae0b26010887e7c9bd8dfc8bf1f673c16e0c4dafec5c"
+      },
+      dob: { date: "1986-04-11T00:43:12Z", age: 33 },
+      registered: { date: "2014-08-02T07:00:32Z", age: 4 },
+      phone: "(062)-663-1647",
+      cell: "(129)-665-1355",
+      id: { name: "SSN", value: "466-35-7679" },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/13.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/13.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/13.jpg"
+      },
+      nat: "US"
+    },
+    {
+      gender: "female",
+      name: { title: "mrs", first: "oya", last: "topaloğlu" },
+      location: {
+        street: "6293 filistin cd",
+        city: "çanakkale",
+        state: "balıkesir",
+        postcode: 18781,
+        coordinates: { latitude: "-30.3190", longitude: "-151.7288" },
+        timezone: {
+          offset: "-6:00",
+          description: "Central Time (US & Canada), Mexico City"
+        }
+      },
+      email: "oya.topaloğlu@example.com",
+      login: {
+        uuid: "20dd911d-40f9-4dd3-bd93-b5ce776416d0",
+        username: "crazypeacock668",
+        password: "redwing",
+        salt: "qbtptwVc",
+        md5: "5793d7bdfc8e299d7e3cc6526125485e",
+        sha1: "85ac7f79ec12dbfcf2e353c59edce796e9463eec",
+        sha256:
+          "78736a4dc57cf74d0783c7cc5e4326e5279a1c4db0b2516e1e9531bf2035905b"
+      },
+      dob: { date: "1958-02-17T06:49:44Z", age: 61 },
+      registered: { date: "2013-04-25T04:22:57Z", age: 6 },
+      phone: "(363)-513-1303",
+      cell: "(120)-030-5940",
+      id: { name: "", value: null },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/8.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/8.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/8.jpg"
+      },
+      nat: "TR"
+    },
+    {
+      gender: "male",
+      name: { title: "mr", first: "neil", last: "johnson" },
+      location: {
+        street: "877 grove road",
+        city: "lisburn",
+        state: "borders",
+        postcode: "F83 1HB",
+        coordinates: { latitude: "16.9499", longitude: "30.1655" },
+        timezone: {
+          offset: "+8:00",
+          description: "Beijing, Perth, Singapore, Hong Kong"
+        }
+      },
+      email: "neil.johnson@example.com",
+      login: {
+        uuid: "7ccfc51f-010d-4774-b4ed-ecadf51cc219",
+        username: "greenbird736",
+        password: "chocolate",
+        salt: "kyhdgNyc",
+        md5: "40d1a25da49df1e23d41ab801ab98e6a",
+        sha1: "ac0c0625b5e18d07e458a2548798deadff5673ee",
+        sha256:
+          "126352a6633bae82c2c5bc21cd6a741771ff54379270ac6c257d20bfecca537a"
+      },
+      dob: { date: "1962-03-24T07:39:09Z", age: 57 },
+      registered: { date: "2010-10-31T09:57:18Z", age: 8 },
+      phone: "015395 69416",
+      cell: "0726-943-975",
+      id: { name: "NINO", value: "BZ 16 52 32 V" },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/76.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/76.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/76.jpg"
+      },
+      nat: "GB"
+    },
+    {
+      gender: "female",
+      name: { title: "miss", first: "michelle", last: "bates" },
+      location: {
+        street: "7464 wycliff ave",
+        city: "hobart",
+        state: "queensland",
+        postcode: 1808,
+        coordinates: { latitude: "-44.8967", longitude: "54.4456" },
+        timezone: {
+          offset: "+3:00",
+          description: "Baghdad, Riyadh, Moscow, St. Petersburg"
+        }
+      },
+      email: "michelle.bates@example.com",
+      login: {
+        uuid: "a63a6b14-eaa9-4259-86ab-59cabfeecc73",
+        username: "organicpeacock819",
+        password: "bingo1",
+        salt: "f9JtAtlm",
+        md5: "82f1fb1e75da3378f317bfc814d26d9f",
+        sha1: "b2cb05a5eada3a22374bd7b1f0ea036ba712a583",
+        sha256:
+          "f60b1a7a1d3af377029d538023165e4312eb1f74cddafe99d0a0d2a7ffd8980a"
+      },
+      dob: { date: "1946-05-06T19:13:54Z", age: 73 },
+      registered: { date: "2002-05-14T07:33:09Z", age: 17 },
+      phone: "05-3383-5348",
+      cell: "0431-545-869",
+      id: { name: "TFN", value: "540581304" },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/5.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/5.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/5.jpg"
+      },
+      nat: "AU"
+    },
+    {
+      gender: "male",
+      name: { title: "mr", first: "آرسین", last: "پارسا" },
+      location: {
+        street: "4725 خرمشهر",
+        city: "ساوه",
+        state: "کهگیلویه و بویراحمد",
+        postcode: 77127,
+        coordinates: { latitude: "23.2677", longitude: "27.7387" },
+        timezone: { offset: "+9:30", description: "Adelaide, Darwin" }
+      },
+      email: "آرسین.پارسا@example.com",
+      login: {
+        uuid: "31140445-5c0b-44ba-8a15-a58263505240",
+        username: "blacksnake517",
+        password: "walmart",
+        salt: "n21hFzj3",
+        md5: "e81669aff6acdcb4a8395a6dc3300c34",
+        sha1: "ecb1ceea094e3ff9535b618665fb0b3a1ec162aa",
+        sha256:
+          "3ef019cf2741d43cc067987b0894323ee0c20df146a1f3ceedfab1ea4d44cfcc"
+      },
+      dob: { date: "1964-10-28T00:08:42Z", age: 54 },
+      registered: { date: "2015-09-26T06:24:20Z", age: 3 },
+      phone: "070-46771232",
+      cell: "0934-124-3221",
+      id: { name: "", value: null },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/69.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/69.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/69.jpg"
+      },
+      nat: "IR"
+    }
+  ],
+  info: { seed: "a3424ee954639857", results: 10, page: 1, version: "1.2" }
+};
